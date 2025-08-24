1f879b498ae6b3ad0853146d1087ac65dca048a3
Hrishikesh Shelke <dev.hrishi@outlook.com>
2025-08-24 21:41:28 +0530

    Mono Repo Build Success, project Infra setup done

---
+++ graphql-gateway/src/main/java/com/quickcommerce/gateway/GraphQLGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.quickcommerce.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLGatewayApplication.class, args);
+    }
+}
